a62370057eabb71fc8f5f7c7e01370333c53ab6b
Nehotieva <104681816+Nehotieva@users.noreply.github.com>
2023-02-03 21:13:54 +0200

    Add files via upload

---
+++ practice2.js
@@ -0,0 +1,33 @@
+let userAge = 20;
+console.log(userAge);
+
+let userName = "Nick";
+console.log(userName);
+
+let userSurname = "Black";
+console.log(userSurname);
+
+let newUser = "Surname";
+console.log(newUser);
+
+let shoppingList = "shoppingList";
+console.log(shoppingList);
+
+const isUserOnline = true;
+
+if (isUserOnline) {
+console.log("isUserOnline");
+} else {
+console.log("isUserOffline");
+}
+
+let employeeMonthlySalary = 30000;
+console.log(employeeMonthlySalary);
+
+let greetings = "Hello World";
+console.log(greetings);
+
+let user = 'John', age = 25, message = 'Hello';
+console.log("John", age, message);
+
+
